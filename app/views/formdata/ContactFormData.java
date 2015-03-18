743e7fe74ac37d635e51506bb01352d52f113cb7
David Neely <davidkneely>
2015-03-17 17:52:04 -1000

    Data would not load over the contact form. Restarted my computer, went through the code line by line. The only thing that fixed it was to run activator clean run. And that fixed it.

---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,13 @@
+package views.formdata;
+
+/**
+ * The backing class for the contact form data.
+ */
+public class ContactFormData {
+  /** The first name. */
+  public String firstName;
+  /** The last name. */
+  public String lastName;
+  /** The telephone number in xxx-xxx-xxxx */
+  public String telephone;
+}
---
+++ app/views/formdata/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by davidneely on 3/17/15.
+ */
+package views.formdata;
