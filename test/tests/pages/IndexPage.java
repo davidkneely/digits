8286211194123916869318d8305e601e4028b98a
David Neely <davidkneely>
2015-03-30 13:09:08 -1000

    Finished WOD.

---
+++ test/tests/pages/IndexPage.java
@@ -0,0 +1,47 @@
+package tests.pages;
+
+import org.fluentlenium.core.FluentPage;
+import org.openqa.selenium.WebDriver;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Provides testing support for the Index page.
+ * @author Philip Johnson
+ */
+public class IndexPage extends FluentPage {
+  private String url;
+  
+  /**
+   * Create the IndexPage.
+   * @param webDriver The driver.
+   * @param port The port.
+   */
+  public IndexPage(WebDriver webDriver, int port) {
+    super(webDriver);
+    this.url = "http://localhost:" + port;
+  }
+  
+  @Override
+  public String getUrl() {
+    return this.url;
+  }
+  
+  @Override
+  public void isAt() {
+    assertThat(title()).isEqualTo("Home Digits");
+  }
+
+  /**
+   * Checks to see that the Index page has the contacts that we input.
+   * @param firstName
+   * @param lastName
+   * @param telephone
+   */
+  public void hasContact(String firstName, String lastName, String telephone) {
+    assertThat(pageSource().contains(firstName));
+    assertThat(pageSource().contains(lastName));
+    assertThat(pageSource().contains(telephone));
+  }
+
+}
---
+++ test/tests/pages/NewContactPage.java
@@ -0,0 +1,48 @@
+package tests.pages;
+
+import org.fluentlenium.core.FluentPage;
+import org.openqa.selenium.WebDriver;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Provides testing support for the Index page.
+ * @author Philip Johnson
+ */
+public class NewContactPage extends FluentPage {
+  private String url;
+
+  /**
+   * Create the IndexPage.
+   * @param webDriver The driver.
+   * @param port The port.
+   */
+  public NewContactPage(WebDriver webDriver, int port) {
+    super(webDriver);
+    this.url = "http://localhost:" + port + "/newcontact";
+  }
+  
+  @Override
+  public String getUrl() {
+    return this.url;
+  }
+  
+  @Override
+  public void isAt() {
+    assertThat(title()).isEqualTo("NewContact Digits");
+  }
+
+  /**
+   * Creates new contact to test against.
+   * @param firstName
+   * @param lastName
+   * @param telephone
+   */
+  public void createContact(String firstName, String lastName, String telephone) {
+    fill("#firstName").with(firstName);
+    fill("#lastName").with(lastName);
+    fill("#telephone").with(telephone);
+    submit("#submit");
+  }
+
+}
